16ef4cafbfa7d9e743deb717bebf76c30c950b2c
gengteng <me@gteng.org>
2021-07-09 17:34:09 +0800

    init workers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use mediasoup::worker::{Worker, WorkerSettings};
+use mediasoup::worker_manager::WorkerManager;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let _workers = init_workers().await?;
+
+    Ok(())
+}
+
+async fn init_workers() -> anyhow::Result<Vec<Worker>> {
+    let cpus = num_cpus::get();
+    let wm = WorkerManager::new();
+
+    let mut vec = Vec::with_capacity(cpus);
+    for _ in 0..cpus {
+        vec.push(wm.create_worker(WorkerSettings::default()).await?);
+    }
+
+    Ok(vec)
 }
